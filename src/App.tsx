80d72fa6b119062ab04a15d9d9789d48ecbed933
joakmannn <165683883+joakmannn@users.noreply.github.com>
2026-01-14 17:09:59 +0100

    add pricing

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,6 @@ import ItemArray from "./components/Item"
 import { itemData } from "./data/itemData"
 
 const App = () => {
-
-/*
- .map sert a parcourir un tableau
- la reflexion pour parcourir un tableau c'est object.map((item) => <Object data={item}>)
- */
-    // Je prends mon tableau 
-    // et j'utilise .map pour parcourir chaque élément du tableau
-    // .map va exécuter une fonction pour chaque élément
-    // et retourner un nouveau tableau
-    // item est la valeur actuelle du tableau pendant le parcours
-    // Chaque item correspond à un element de mon tableau
-    // Il est ensuite utilisé pour générer un composant dynamique
-    /// ----> Etat de l'art 
-
-    ///objectData.map((k) => <object data= {k}/>)
-
-
   return (
          
          
@@ -31,8 +14,6 @@ const App = () => {
 
               ))
             }          
-
-
         </div>
   )
 }
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,41 +1,39 @@
 import type { IItem } from "../interfaces/item"
 
 
-    interface IItemComponent {
-        itemInfos: IItem,
-    }
+interface IItemComponent {
+    itemInfos: IItem,
+}
 
-    const ItemArray = ({itemInfos}: IItemComponent) => {
-    console.log("🚀 ~ ItemComponent ~ ItemComponent:", ItemArray)
-     
-    
-    
+const ItemArray = ({ itemInfos }: IItemComponent) => {
 
-    return(
+
+
+
+    return (
         <div className="card-item border-2 p-4 rounded-2xl mb-1.5">
             <div className="mb-4 flex flex-col items-center">
-                <img width={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3GpL_ZIDGgvkK7A_dISo4-88IOsTXbVWxPw&s" alt="" className="rounded-2xl" />
-            
-            <p className="ml-auto rounded-2xl border mt-2 px-3 py-1">{itemInfos.product_price}</p>
-
-               
+                <img width={300} src={itemInfos.product_path}
+                    alt="" className="rounded-2xl"
+                />
+                <p className="ml-auto rounded-2xl border mt-2 px-3 py-1">{itemInfos.product_price}</p>
             </div>
             <div className="border rounded-2xl pl-4">
                 <h3 className="text-3xl mt-4 mb-">{itemInfos.product_name}</h3>
                 <div className="mb-4 mr-4 ml-4 mt-4">
-                <p>{itemInfos.product_description}</p>
+                    <p>{itemInfos.product_description}</p>
 
                 </div>
             </div>
             <div className="flex justify-center mt-4">
-            <button className="btn btn-secondary">Add to card</button>
-                
+                <button className="btn btn-secondary">Add to card</button>
+
             </div>
-            
+
 
 
 
         </div>
     )
-} 
+}
 export default ItemArray;
