a4f898deebf7ef0121fbe22b3c9faa7f0b12565f
joakmannn <165683883+joakmannn@users.noreply.github.com>
2026-01-14 15:10:23 +0100

    create mocked data and type interface

---
+++ src/data/itemData.ts
@@ -0,0 +1,23 @@
+import type { IItem } from "../interfaces/item"
+
+export const itemData: IItem[] = [
+    {
+        product_name: "T-shirt",
+        product_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem a necessitatibus placeat at quos consequuntur deserunt nemo veniam non sapiente qui, repellendus aspernatur enim illum corporis minus numquam suscipit fugiat.",
+        product_path: "https://image.spreadshirtmedia.com/image-server/v1/products/T1647A2PA4195PT17X8Y30D1057690672W34990H35760/views/1,width=378,height=378,appearanceId=2,backgroundColor=F2F2F2,modelId=6069,crop=list/teaching-irl-and-url-influencer-teacher-t-shirt.jpg",
+        product_price: 27.99,
+
+    },
+
+    {
+
+        product_name: "Veste",
+        product_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem a necessitatibus placeat at quos consequuntur deserunt nemo veniam non sapiente qui, repellendus aspernatur enim illum corporis minus numquam suscipit fugiat.",
+        product_path: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3GpL_ZIDGgvkK7A_dISo4-88IOsTXbVWxPw&s",
+        product_price: 157.99,
+
+    }
+
+
+]
+
---
+++ src/interfaces/item.ts
@@ -0,0 +1,6 @@
+ export interface IItem {
+        product_name: string,
+        product_description: string,
+        product_path: string,
+        product_price: number,
+ }
