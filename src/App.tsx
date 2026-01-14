0728cf16553b43030c61e527d54ac9e911baf518
joakmannn <165683883+joakmannn@users.noreply.github.com>
2026-01-14 16:01:34 +0100

    add props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import { Item } from "./components/Item"
+import ItemArray from "./components/Item"
+import { itemData } from "./data/itemData"
+
 const App = () => {
 
+/*
+ .map sert a parcourir un tableau
+ la reflexion pour parcourir un tableau c'est object.map((item) => <Object data={item}>)
+ */
+    // Je prends mon tableau 
+    // et j'utilise .map pour parcourir chaque élément du tableau
+    // .map va exécuter une fonction pour chaque élément
+    // et retourner un nouveau tableau
+    // item est la valeur actuelle du tableau pendant le parcours
+    // Chaque item correspond à un element de mon tableau
+    // Il est ensuite utilisé pour générer un composant dynamique
+    /// ----> Etat de l'art 
+
+    ///objectData.map((k) => <object data= {k}/>)
+
+
   return (
+         
+         
          <div className="gap-3 grid grid-cols-4 ">
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          
+            {
+              itemData.length && itemData.map((item, index) => (
+                <div>
+                  <ItemArray key={index} itemInfos={item} />
+                </div>
+
+              ))
+            }          
 
 
         </div>
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,17 +1,29 @@
-export function Item () {
+import type { IItem } from "../interfaces/item"
+
+
+    interface IItemComponent {
+        itemInfos: IItem,
+    }
+
+    const ItemArray = ({itemInfos}: IItemComponent) => {
+    console.log("🚀 ~ ItemComponent ~ ItemComponent:", ItemArray)
+     
+    
+    
+
     return(
         <div className="card-item border-2 p-4 rounded-2xl mb-1.5">
             <div className="mb-4 flex flex-col items-center">
                 <img width={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3GpL_ZIDGgvkK7A_dISo4-88IOsTXbVWxPw&s" alt="" className="rounded-2xl" />
             
-            <p className="ml-auto rounded-2xl border mt-2 px-3 py-1">Price</p>
+            <p className="ml-auto rounded-2xl border mt-2 px-3 py-1">{itemInfos.product_price}</p>
 
                
             </div>
             <div className="border rounded-2xl pl-4">
-                <h3 className="text-3xl mt-4 mb-">Item name</h3>
+                <h3 className="text-3xl mt-4 mb-">{itemInfos.product_name}</h3>
                 <div className="mb-4 mr-4 ml-4 mt-4">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem a necessitatibus placeat at quos consequuntur deserunt nemo veniam non sapiente qui, repellendus aspernatur enim illum corporis minus numquam suscipit fugiat.</p>
+                <p>{itemInfos.product_description}</p>
 
                 </div>
             </div>
@@ -25,4 +37,5 @@ export function Item () {
 
         </div>
     )
-}
+} 
+export default ItemArray;
