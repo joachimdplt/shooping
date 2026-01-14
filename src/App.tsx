78303639d14ea7a5447568205cd5826e1ac71dab
joakmannn <165683883+joakmannn@users.noreply.github.com>
2026-01-14 14:38:09 +0100

    add basic render to items

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import { Item } from "./components/Item"
 const App = () => {
 
   return (
-    <div>
-      <Item />
-    
-    </div>
+         <div className="gap-3 grid grid-cols-4 ">
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          
+
+
+        </div>
   )
 }
 export default App
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,7 +1,28 @@
 export function Item () {
     return(
-        <div>
-            Hello World from Item component
+        <div className="card-item border-2 p-4 rounded-2xl mb-1.5">
+            <div className="mb-4 flex flex-col items-center">
+                <img width={300} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3GpL_ZIDGgvkK7A_dISo4-88IOsTXbVWxPw&s" alt="" className="rounded-2xl" />
+            
+            <p className="ml-auto rounded-2xl border mt-2 px-3 py-1">Price</p>
+
+               
+            </div>
+            <div className="border rounded-2xl pl-4">
+                <h3 className="text-3xl mt-4 mb-">Item name</h3>
+                <div className="mb-4 mr-4 ml-4 mt-4">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem a necessitatibus placeat at quos consequuntur deserunt nemo veniam non sapiente qui, repellendus aspernatur enim illum corporis minus numquam suscipit fugiat.</p>
+
+                </div>
+            </div>
+            <div className="flex justify-center mt-4">
+            <button className="btn btn-secondary">Add to card</button>
+                
+            </div>
+            
+
+
+
         </div>
     )
 }
